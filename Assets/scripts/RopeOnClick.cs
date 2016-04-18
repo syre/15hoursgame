d07952d79e8490769b14559f5d0133c7b87add14
Søren Howe Gersager <syrelyre@gmail.com>
2016-04-18 15:07:16 +0200

    added ropeonclick

---
+++ Assets/scripts/RopeOnClick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddRope : MonoBehaviour {
+	public HingeJoint2D joint;
+	public float ropeDistance = 10f;
+
+	// Use this for initialization
+	void Start () {
+		joint = gameObject.AddComponent<HingeJoint2D> ();
+		joint.enabled = false;
+	}
+	void OnMouseDown()
+	{
+		Vector2 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		joint.enabled = true;
+		var ninjaPos = gameObject.GetComponent<Rigidbody2D> ();
+		var castResult = Physics2D.Raycast(ninjaPos.position, mouseWorldPosition, ropeDistance);
+		if (castResult.collider.tag == "ropeable")
+		{
+			joint.connectedAnchor = castResult.collider.attachedRigidbody.position;
+			joint.connectedBody = ninjaPos;
+		}
+	}
+	void OnMouseUp()
+	{
+		joint.enabled = false;
+	}
+}

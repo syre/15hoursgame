6f7219d9e9316ec04240de81cb706e66e4d9d039
Mortenbaws <morten2094@hotmail.com>
2016-04-18 23:57:18 +0200

    fixed rope and made fatty

---
+++ Assets/scripts/LineRenderScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineRenderScript : MonoBehaviour {
+
+	public LineRenderer line;
+	public GameObject manpoint;
+	public GameObject hook;
+	// Use this for initialization
+	void Start () {
+		line.SetVertexCount (2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		line.SetPosition (0, manpoint.transform.position);
+		line.SetPosition (1, hook.transform.position);
+	}
+}
--- Assets/scripts/RopeOnClick.cs
+++ Assets/scripts/RopeOnClick.cs
@@ -6,10 +6,25 @@ public class RopeOnClick : MonoBehaviour {
 	public FixedJoint2D jointTarget;
 	public float ropeDistance = 10f;
 	public GameObject castpoint;
+	//public GameObject part5;
+	//public GameObject part6;
+	//public GameObject part7;
+	//public GameObject part8;
+	public LineRenderer line;
+	public GameObject connectPoint;
+	public GameObject hook;
+	private bool isFat = false;
+	public Sprite thinGuy;
+	public Sprite fatGuy;
+	public DistanceJoint2D joint;
 
 	// Use this for initialization
 	void Start () {
 		detachRope ();
+		line.SetVertexCount (2);
+		//part6.SetActive (false);
+		//part7.SetActive (false);
+		//part8.SetActive (false);
 	}
 	void attachRope()
 	{
@@ -20,24 +35,39 @@ public class RopeOnClick : MonoBehaviour {
 		if (castResult.collider != null && castResult.collider.gameObject != null) {
 			if (castResult.collider.gameObject.tag == "ropeable")
 			{
-				Vector2 oldpos = jointTarget.connectedAnchor;
+				line.enabled = true;
 				jointTarget.gameObject.SetActive (true);
 				this.transform.GetComponent<HingeJoint2D> ().enabled = true;
 				jointTarget.connectedAnchor = castResult.point;
 			}
 		}
 
+
 	}
 	void Update()
 	{
+		line.SetPosition (0, connectPoint.transform.position);
+		line.SetPosition (1, castpoint.transform.position);
 		if (Input.GetMouseButtonDown (0))
 			attachRope();
 		else if (Input.GetMouseButtonUp(2))
 			detachRope();
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (isFat) {
+				isFat = false;
+				joint.distance = 3;
+				this.transform.GetComponent<SpriteRenderer> ().sprite = thinGuy;
+			} else {
+				isFat = true;
+				joint.distance = 6;
+				this.transform.GetComponent<SpriteRenderer> ().sprite = fatGuy;
+			}
+		}
 	}
 	
 	void detachRope()
 	{
+		line.enabled = false;
 		jointTarget.enabled = false;
 		this.transform.GetComponent<HingeJoint2D> ().enabled = false;
 		jointTarget.gameObject.SetActive (false);

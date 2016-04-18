3565f1374b73f303a2c4a1d4b940be23f790af71
Mortenbaws <morten2094@hotmail.com>
2016-04-18 14:23:51 +0200

    deathscript and colliderninujanotfin

---
+++ Assets/scripts/DeathScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DeathScript : MonoBehaviour {
+
+	public Text diedText;
+	public Button tryAgainButton;
+
+	// Use this for initialization
+	void Start () {
+		diedText.enabled = false;
+		tryAgainButton.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void OnDeath(){
+		diedText.enabled = true;
+		tryAgainButton.gameObject.SetActive (true);
+	}
+
+	public void PlayAgain(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	}
+
+
+
+
+}
